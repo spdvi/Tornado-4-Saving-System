a54b2953c177346f22da902b0feac7126f242c5c
spdvi <gjm@paucasesnovescifp.cat>
2024-12-18 11:18:13 +0100

    Serialize Vector 3. Added JsonStatics. Now we can do transform.potision.ToToken()

--- Assets/Scripts/FPS/PlayerFPSMove.cs
+++ Assets/Scripts/FPS/PlayerFPSMove.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace Tornado4.Player
 {
-    public class PlayerFPSMove : MonoBehaviour
+    public class PlayerFPSMove : MonoBehaviour, IJsonSaveable
     {
         public float speed = 2.0f;
 
@@ -15,6 +16,17 @@ namespace Tornado4.Player
 
             transform.Translate(horizontal * Time.deltaTime * speed, 0,
                 vertical * Time.deltaTime * speed);
+        }
+
+        public JToken CaptureAsJToken()
+        {
+            JToken positionJToken = transform.position.ToToken();
+            return positionJToken;
+        }
+
+        public void RestoreFromJToken(JToken state)
+        {
+            transform.position = state.ToVector3();
         }
     }
 }
